8265ffe9dedcbcfb6631289937818e2bf9d6aa4d
Ectario <dimitritristangalaxy@gmail.com>
2021-06-09 17:54:21 +0200

    Read file and get binary**

--- CryXor/CryXor.cpp
+++ CryXor/CryXor.cpp
@@ -1,10 +1,47 @@
-// CryXor.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
+#include <fstream>
+#include <string>
 #include <iostream>
+#include <bitset>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+string TextToBinaryString(string words) {
+    string binaryString = "";
+    for (char& _char : words) {
+        binaryString += bitset<8>(_char).to_string();
+    }
+    return binaryString;
 }
+
+
+int main() {
+    //// Create a text file
+    //ofstream MyWriteFile("../texte.txt");
+
+    //// Write to the file
+    //MyWriteFile << "Files can be tricky, but it is fun enough!";
+
+    //// Close the file
+    //MyWriteFile.close();
+
+    // Create a text string, which is used to output the text file
+    string myText;
+    string myLine;
+
+
+    // Read from the text file
+    ifstream MyReadFile("../texte.txt");
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline(MyReadFile, myLine)) {
+        // Output the text from the file
+        myText += myLine;
+    }
+
+    cout << TextToBinaryString(myText);
+
+    // Close the file
+    MyReadFile.close();
+}
+
 
---
+++ CryXor/FileManager.h
@@ -0,0 +1,12 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include <string>
+using namespace std;
+
+class FileManager {
+public:
+	FileManager(string path);
+};
+
+#endif //FILEMANAGER_H
