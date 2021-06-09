68138585a61279e26c2083ab456819d608cf1605
Ectario <dimitritristangalaxy@gmail.com>
2021-06-09 17:40:28 +0200

    Revert "Read file and get binary"
    This reverts commit 9ec0875e2a2b9f36937c4a799465686246b06d1d.

--- CryXor/CryXor.cpp
+++ CryXor/CryXor.cpp
@@ -1,38 +1,10 @@
+// CryXor.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <bitset>
 
-using namespace std;
-
-//texte.txt is in ASCII !
-string TextToBinaryString(string words) {
-	string binaryString = "";
-	for (char& _char : words) {
-		binaryString += bitset<8>(_char).to_string();
-	}
-	return binaryString;
+int main()
+{
+    std::cout << "Hello World!\n";
 }
 
-
-int main() {
-	// Create and open a text file
-	ifstream MyFile("../texte.txt");
-
-	// Create a text string, which is used to output the text file
-	string myText;
-	string myLine;
-
-	// Use a while loop together with the getline() function to read the file line by line
-	while (getline(MyFile, myLine)) {
-		// Output the text from the file
-		myText+=myLine;
-	}
-	
-	cout << TextToBinaryString(myText);
-
-	// Close the file
-	MyFile.close();
-
-	return EXIT_SUCCESS;
-}
