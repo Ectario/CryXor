fd2a4367c180b09683789588405011c25cb48574
Ectario <dimitritristangalaxy@gmail.com>
2021-06-09 17:38:25 +0200

    Read file and get binary

---
+++ CryXor/FileManager.h
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <string>
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+using namespace std;
+
+class FileManager {
+public:
+	FileManager(string path);
+
+};
+
+
+
+
+#endif // FILEMANAGER_H
